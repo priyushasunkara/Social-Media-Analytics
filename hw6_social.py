987d1e8553c3bd915af2ad7530ffcffd984cfd83
priyusha99 <sunkarapriyusha1999@gmail.com>
2021-10-29 10:06:15 +0530

    code implemented graphHashtagSentimentByFrequency

--- hw6_social.py
+++ hw6_social.py
@@ -341,6 +341,16 @@ Parameters: dataframe
 Returns: None
 '''
 def graphHashtagSentimentByFrequency(data):
+    dict1=getHashtagRates(data)
+    mostcommon=mostCommonHashtags(dict1,50)
+    hashtaglst=[]
+    freqlst=[]
+    sentimentlst=[]
+    for i in mostcommon:
+        hashtaglst.append(i)
+        freqlst.append(mostcommon[i])
+        sentimentlst.append(getHashtagSentiment(data,i))
+    scatterPlot(freqlst,sentimentlst,hashtaglst,"Hashtags Frequency")
     return
 
 
